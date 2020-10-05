f31e07123ac360b46bfe531580a6a4bc0834b3dc
blairtyx <70720102+blairtyx@users.noreply.github.com>
2020-10-05 21:48:14 +0800

    Project 2 Application mainbody finished. README.md need update

--- Project2/application/test.py
+++ Project2/application/test.py
@@ -5,6 +5,8 @@ import pandas as pd
 import twitter_credentials as tc
 from google.cloud import language_v1
 from google.cloud.language_v1 import enums
+import matplotlib_terminal
+import matplotlib.pyplot as plt
 
 
 # Anuthenticaiton
@@ -15,10 +17,8 @@ api = tweepy.API(auth)
 
 
 # read keywords from shell input
-screen_name = input("Whose twitter do you want to analysis? (type the user name e.g. JoeBiden, realDonaldTrump) ")
-count = int(input("How many tweets do you need? (range 10-1000) "))
-rts = input("Do you need retweets? (type <true> if you do) ")
-rpls = not input("Do you need replies? (type <ture> if you do) ")
+# screen_name = input("Whose twitter do you want to analysis? (type the user name e.g. JoeBiden, realDonaldTrump) ")
+
 
 
 
@@ -26,13 +26,54 @@ rpls = not input("Do you need replies? (type <ture> if you do) ")
 #user input test
 
 ## username test
+while True:
+  screen_name = input("Whose twitter do you want to analysis? (type the user name e.g. JoeBiden, realDonaldTrump) ")
+  if re.search(r'[\s]', screen_name):
+    print ("    No spaces please.")
+  else:
+    break
+
 
 ## count test
+while True:
+  count = input("How many tweets do you need? (range 10-1000) ")
+  if (int(count) in range(10,1001)):
+    count = int(count)
+    break
+  else:
+    print ("    Please pick a integer between 10-1000")
+True
 
 ## retweets test
+rts_list_1 = ['1', 'True', 'true', 'TRUE','yes', 'YES']
+rts_list_0 = ['0', 'False', 'false','FALSE', 'no', 'NO']
+while True:
+  rts = input("Do you need retweets? (type <true> if you do) ")
+  if (rts in rts_list_1):
+    rts = 'True'
+    break
+  elif (rts in rts_list_0):
+    rts = 'False'
+    break
+  else:
+    print ("    Please type <true>, or <false>")
+
+
 
 ## reply test
 
+rpls_list_1 = ['1', 'True', 'true', 'TRUE','yes', 'YES']
+rpls_list_0 = ['0', 'False', 'false','FALSE','no', 'NO']
+while True:
+  rpls = input("Do you need replies? (type <ture> if you do) ")
+  if (rpls in rpls_list_1):
+    rpls = 'False'
+    break
+  elif (rpls in rpls_list_0):
+    rpls = 'True'
+    break
+  else:
+    print ("    Please type <true>, or <false>")
 
 
 
@@ -41,14 +82,17 @@ posts = api.user_timeline(screen_name = screen_name,
                                     count = count, 
                                     lang = "en", 
                                     tweet_mode = "extended",
-                                    include_rts = rts,
+                                    include_rpls = rpls,
                                     exclude_replies = rpls )
 
 
 
 #create a dataframe with a column called Tweets
+print ()
+print ('Retrieving data from Twitter API......')
 df = pd.DataFrame([tweet.full_text for tweet in posts], columns = ['Tweets'])
-print (df)
+print ('  Data received!  ')
+print()
 
 
 
@@ -72,20 +116,6 @@ def analyzeSentiment(text):
     encoding_tpye = enums.EncodingType.UTF8
 
     response = client.analyze_sentiment(document, encoding_type=encoding_tpye)
-
-    # print()
-    # print(u"Document Sentiment Score: {}".format(response.document_sentiment.score))
-    # print(u"Document Sentiment magnitude: {}".format(response.document_sentiment.magnitude))
-    # score = format(response.document_sentiment.score)
-    # mag = format(response.document_sentiment.magnitude)
-    # df['Document_Sentiment_Score'].append(score)
-    # df['Document_Sentiment_magnitude'].append(mag)
-    # for sentence in response.sentences:
-    #     print(u"Sentence test: {}".format(sentence.text.content))
-    #     print(u"Sentence sentiment score: {}".format(sentence.sentiment.score))
-    #     print(u"Sentence sentiment magnitude: {}".format(sentence.sentiment.magnitude))
-
-    # print(u"Language of the text: {}".format(response.language))
     return response
 
 
@@ -94,31 +124,65 @@ def analyzeSentiment(text):
 
 
 #clean text of tweets 
+print ('Cleaning text content......')
+
 df['Tweets'] = df['Tweets'].apply(cleanText)
-print (df)
+print ('  Cleaning process finished!')
+print ()
 
 
-
-#main part
+#Google NLP API & data formating
+print('Conneting to Google NLP API ......')
 for i in range(0,df.shape[0]):
     #retrive analysis resopnse
     response = analyzeSentiment(df.at[i,'Tweets'])
-    
+    print ('  Received data from Google NLP API, processing......')
     #format output
     df.at[i,'D_score'] = response.document_sentiment.score
     df.at[i,'D_magnitude'] = response.document_sentiment.magnitude
     df.at[i,'Response'] = response
-    
+print ("  Sentiment analysis finished!")    
+print()
 
 
-#test part
+def getAnalysis(score):
+  if score < -0.1:
+    return 'Negative'
+  elif ((score >= -0.1) & (score <= 0.1) ):
+    return 'Neutral'
+  else: 
+    return 'Positive'
+
+
+print ('Data formating......')
+df['Analysis'] = df['D_score'].apply(getAnalysis)
+print ('  Data format finished!')
+print ()
+
+#show the distribution of all tweets been analyzed
+print ('Printing out Plots of the analysis result')
 print()
+plt.figure(figsize=(6,6))
+for i in range(0,df.shape[0]):
+    plt.scatter(df.at[i, 'D_score'], df.at[i, 'D_magnitude'])
+plt.title('Sentiment Analysis')
+plt.xlabel('Score')
+plt.ylabel('Magnitude')
+plt.show()
+plt.close()
+
+# show a bar chart of Neg/Neu/Pos
 print()
+print(df['Analysis'].value_counts())
 print()
-print("Show the df['D_score'] ")
-print (df['D_score'])
-print()
-print("Show the df['D magnitude'] ")
-print (df['D_magnitude'])
-print()
-print(df)
+plt.figure(figsize=(6,6))
+
+plt.title('Sentiment Analysis')
+plt.xlabel('Sentiment')
+plt.ylabel('Counts')
+df['Analysis'].value_counts().plot(kind = 'bar')
+plt.show()
+plt.close()
+
+
+
