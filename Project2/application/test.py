172a13eba64f81b1248fa53b2f42a5ffb8a2f22f
blairtyx <70720102+blairtyx@users.noreply.github.com>
2020-12-09 03:08:08 +0800

    Update

--- Project2/application/test.py
+++ Project2/application/test.py
@@ -7,12 +7,16 @@ from google.cloud.language_v1 import enums
 import matplotlib_terminal
 import matplotlib.pyplot as plt
 import os
+import argparse
 
 # Anuthenticaiton
-auth = tweepy.OAuthHandler(os.getenv('CONSUMER_KEY'), os.getenv('CONSUMER_SECRET'))
-auth.set_access_token(os.getenv('ACCESS_TOKEN'), os.getenv('tc.ACCESS_TOKON_SECRET'))
+def authu():
 
-api = tweepy.API(auth)
+  auth = tweepy.OAuthHandler(os.getenv('CONSUMER_KEY'), os.getenv('CONSUMER_SECRET'))
+  auth.set_access_token(os.getenv('ACCESS_TOKEN'), os.getenv('tc.ACCESS_TOKON_SECRET'))
+
+  api = tweepy.API(auth)
+  return api
 
 
 # read keywords from shell input
@@ -23,75 +27,72 @@ api = tweepy.API(auth)
 
 #################################
 #user input test
+def user_input_info(name_i, cnt_i, rts_i, rpls_i, api ):
+  ## username test
 
-## username test
-while True:
-  screen_name = input("Whose twitter do you want to analysis? (type the user name e.g. JoeBiden, realDonaldTrump) ")
-  if re.search(r'[\s]', screen_name):
+  if re.search(r'[\s]', name_i):
     print ("    No spaces please.")
-  else:
-    break
+    return 1
 
-
-## count test
-while True:
-  count = input("How many tweets do you need? (range 10-1000) ")
-  if (int(count) in range(10,1001)):
-    count = int(count)
-    break
+  ## count test
+  if (int(cnt_i) in range(10,1001)):
+    count = int(cnt_i)
   else:
     print ("    Please pick a integer between 10-1000")
-True
+    return 2
 
-## retweets test
-rts_list_1 = ['1', 'True', 'true', 'TRUE','yes', 'YES', 'Yes', '<true>']
-rts_list_0 = ['0', 'False', 'false','FALSE', 'no', 'NO', 'No', '<false>']
-while True:
-  rts = input("Do you need retweets? (type <true> if you do) ")
-  if (rts in rts_list_1):
+  ## retweets test
+  rts_list_1 = ['1', 'True', 'true', 'TRUE','yes', 'YES', 'Yes', '<true>']
+  rts_list_0 = ['0', 'False', 'false','FALSE', 'no', 'NO', 'No', '<false>']
+  if (rts_i in rts_list_1):
     rts = 'True'
-    break
-  elif (rts in rts_list_0):
+  elif (rts_i in rts_list_0):
     rts = 'False'
-    break
   else:
     print ("    Please type <true>, or <false>")
+    return 3
 
 
-
-## reply test
-
-rpls_list_1 = ['1', 'True', 'true', 'TRUE','yes', 'YES', 'Yes', '<true>']
-rpls_list_0 = ['0', 'False', 'false','FALSE','no', 'NO', 'No', '<false>']
-while True:
-  rpls = input("Do you need replies? (type <ture> if you do) ")
-  if (rpls in rpls_list_1):
+  ## reply test
+  rpls_list_1 = ['1', 'True', 'true', 'TRUE','yes', 'YES', 'Yes', '<true>']
+  rpls_list_0 = ['0', 'False', 'false','FALSE','no', 'NO', 'No', '<false>']
+  if (rpls_i in rpls_list_1):
     rpls = 'False'
-    break
-  elif (rpls in rpls_list_0):
+  elif (rpls_i in rpls_list_0):
     rpls = 'True'
-    break
   else:
     print ("    Please type <true>, or <false>")
+    return 4
+  
+  
 
+  #Retreive tweets from twitter
+  posts = api.user_timeline(screen_name = name_i, 
+                                      count = count, 
+                                      lang = "en", 
+                                      tweet_mode = "extended",
+                                      include_rts = rts,
+                                      exclude_replies = rpls )
 
-
-#Retreive tweets from twitter
-posts = api.user_timeline(screen_name = screen_name, 
-                                    count = count, 
-                                    lang = "en", 
-                                    tweet_mode = "extended",
-                                    include_rpls = rpls,
-                                    exclude_replies = rpls )
+  return posts
 
 
 
 #create a dataframe with a column called Tweets
-print ()
-print ('Retrieving data from Twitter API......')
-df = pd.DataFrame([tweet.full_text for tweet in posts], columns = ['Tweets'])
-print ('  Data received!  ')
-print()
+def tweets_storing(posts):
+  print ()
+  print ('Retrieving data from Twitter API......')
+  df = pd.DataFrame([tweet.full_text for tweet in posts], columns = ['Tweets'])
+  print ('  Data received!  ')
+  print()
+
+  #clean text of tweets 
+  print ('Cleaning text content......')
+
+  df['Tweets'] = df['Tweets'].apply(cleanText)
+  print ('  Cleaning process finished!')
+  print ()
+  return df
 
 
 
@@ -118,30 +119,20 @@ def analyzeSentiment(text):
     return response
 
 
-
-
-
-
-#clean text of tweets 
-print ('Cleaning text content......')
-
-df['Tweets'] = df['Tweets'].apply(cleanText)
-print ('  Cleaning process finished!')
-print ()
-
-
 #Google NLP API & data formating
-print('Conneting to Google NLP API ......')
-for i in range(0,df.shape[0]):
-    #retrive analysis resopnse
-    response = analyzeSentiment(df.at[i,'Tweets'])
-    print ('  Received data from Google NLP API, processing......')
-    #format output
-    df.at[i,'D_score'] = response.document_sentiment.score
-    df.at[i,'D_magnitude'] = response.document_sentiment.magnitude
-    df.at[i,'Response'] = response
-print ("  Sentiment analysis finished!")    
-print()
+def google_nlp(df):
+  print('Conneting to Google NLP API ......')
+  for i in range(0,df.shape[0]):
+      #retrive analysis resopnse
+      response = analyzeSentiment(df.at[i,'Tweets'])
+      print ('  Received data from Google NLP API, processing......')
+      #format output
+      df.at[i,'D_score'] = response.document_sentiment.score
+      df.at[i,'D_magnitude'] = response.document_sentiment.magnitude
+      df.at[i,'Response'] = response
+  print ("  Sentiment analysis finished!")    
+  print()
+
 
 
 def getAnalysis(score):
@@ -153,35 +144,60 @@ def getAnalysis(score):
     return 'Positive'
 
 
-print ('Data formating......')
-df['Analysis'] = df['D_score'].apply(getAnalysis)
-print ('  Data format finished!')
-print ()
-
-#show the distribution of all tweets been analyzed
-print ('Printing out Plots of the analysis result')
-print()
-plt.figure(figsize=(6,6))
-for i in range(0,df.shape[0]):
-    plt.scatter(df.at[i, 'D_score'], df.at[i, 'D_magnitude'])
-plt.title('Sentiment Analysis')
-plt.xlabel('Score')
-plt.ylabel('Magnitude')
-plt.show()
-plt.close()
-
-# show a bar chart of Neg/Neu/Pos
-print()
-print(df['Analysis'].value_counts())
-print()
-plt.figure(figsize=(6,6))
-
-plt.title('Sentiment Analysis')
-plt.xlabel('Sentiment')
-plt.ylabel('Counts')
-df['Analysis'].value_counts().plot(kind = 'bar')
-plt.show()
-plt.close()
+def nlp_analysis(df):
+  print ('Data formating......')
+  df['Analysis'] = df['D_score'].apply(getAnalysis)
+  print ('  Data format finished!')
+  print ()
 
 
+def result_represent(df):
+  #show the distribution of all tweets been analyzed
+  print ('Printing out Plots of the analysis result')
+  print()
+  plt.figure(figsize=(6,6))
+  for i in range(0,df.shape[0]):
+      plt.scatter(df.at[i, 'D_score'], df.at[i, 'D_magnitude'])
+  plt.title('Sentiment Analysis')
+  plt.xlabel('Score')
+  plt.ylabel('Magnitude')
+  plt.show()
+  plt.close()
+
+  # show a bar chart of Neg/Neu/Pos
+  print()
+  print(df['Analysis'].value_counts())
+  print()
+  plt.figure(figsize=(6,6))
+
+  plt.title('Sentiment Analysis')
+  plt.xlabel('Sentiment')
+  plt.ylabel('Counts')
+  df['Analysis'].value_counts().plot(kind = 'bar')
+  plt.show()
+  plt.close()
+
+  return
+
+
+def main(name, count, rts_flag, rpls_flag):
+  api = authu()
+  posts = user_input_info(name, count, rts_flag, rpls_flag, api)
+  data_frame = tweets_storing(posts)
+  google_nlp(data_frame)
+  nlp_analysis(data_frame)
+  result_represent(data_frame)
+  return
+  
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description='Sentiment Analyzer for Tweets, using Google NLP')
+  parser.add_argument('--twitter-name', type=str, dest='name', default='Twitter')
+  parser.add_argument('--tweets-num', type=int, dest='number', default='10')
+  parser.add_argument('--retweets', type=str, dest='rts', default=False)
+  parser.add_argument('--replies', type=str, dest='rpls', default=False)
+  args = parser.parse_args()
+  main(args.name, args.number, args.rts, args.rpls)
+
+  pass
 
